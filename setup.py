65a407047de2fcf452237235352c99b7e05c27c3
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2025-03-06 12:45:57 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyfresh',
+   version='1.0.0',
+   description='Sample module',
+   author='Renat',
+   author_email='road10mc@gmail.com',
+   packages=['terminal', 'lib', 'tasks'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
