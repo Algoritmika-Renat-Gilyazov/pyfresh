813c2f87db4b8477e7570da83e178169bc7700e0
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2025-03-06 20:44:34 +0500

    Update usable.py

--- lib/usable.py
+++ lib/usable.py
@@ -3,11 +3,11 @@ def double(x):
 class Player:
   def __init__(self):
     self.health = 100
-    self.attack = 5
+    self.attackv = 5
   def attack(self, target: Player):
     target.update(health=target.health-self.attack)
   def regen(self):
     self.update(health=self.health+10)
   def update(self, **kwargs):
     self.health = kwargs.get("health", self.health)
-    self.attack = kwargs.get("attack", self.attack)
+    self.attackv = kwargs.get("attack", self.attack)
