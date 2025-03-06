3eb952a6e42eade3df7cdc21115494d5cf1c2495
Algoritmika-Renat-Gilyazov <road10mc@gmail.com>
2025-03-06 12:51:45 +0500

    Create main.py

---
+++ terminal/main.py
@@ -0,0 +1,10 @@
+class Terminal:
+  """Terminal"""
+  @classmethod
+  def echo(text):
+    """print(text)"""
+    print(text)
+  @classmethod
+  def get(msg):
+    """input(msg)"""
+    return input(msg)
